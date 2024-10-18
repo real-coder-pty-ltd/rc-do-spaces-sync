aa58944d4d1ebdf130f24f22253b0fe5fbb6924b
Matthew Neal <68095856+mattneal-stafflink@users.noreply.github.com>
2024-10-18 15:20:02 +1000

    Update Space.php

--- frappacchio/DOSpaces/Space.php
+++ frappacchio/DOSpaces/Space.php
@@ -3,24 +3,21 @@
 namespace frappacchio\DOSpaces;
 
 use Aws\S3\S3Client;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 use League\Flysystem\Filesystem as FS;
 
 /**
  * Class Space
  *
  * Create e Space instance in order to connect to DOSpaces
- *
- * @package frappacchio\DOSpaces
  */
 class Space
 {
     /**
-     * @param string $key container key
-     * @param string $secret container secret
-     * @param string $container container name
-     * @param string $endpoint container endpoint (including scheme)
-     *
+     * @param  string  $key  container key
+     * @param  string  $secret  container secret
+     * @param  string  $container  container name
+     * @param  string  $endpoint  container endpoint (including scheme)
      * @return FS
      */
     public static function getInstance($key, $secret, $container, $endpoint)
@@ -36,7 +33,7 @@ class Space
             'region' => 'us-east-1',
         ]);
 
-        $connection = new AwsS3Adapter($client, $container);
+        $connection = new AwsS3V3Adapter($client, $container);
         $filesystem = new FS($connection);
 
         return $filesystem;
